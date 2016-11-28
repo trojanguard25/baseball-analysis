02ffc430aa7dbce956998c3c9efcd322dd0a6c30
Dan Rausch <dan.rausch@gmail.com>
2016-11-27 22:51:30 -0500

    Added task to get the age for each pro season.

--- luigi_tasks.py
+++ luigi_tasks.py
@@ -7,19 +7,68 @@ sys.path.append('/home/dan/github/player-finder/src/')
 from baseballReferenceSource import BRSource
 from config import Config
 
+class AgeSeasons(luigi.Task):
+    uuid = luigi.Parameter()
+
+    def run(self):
+        conn = pymysql.connect(host=Config.dbHost, port=Config.dbPort, user=Config.dbUser, passwd=Config.dbPass, db=Config.dbName)
+
+        sql = 'select birth_year, birth_month, birth_day, pro_played_first, pro_played_last'
+        sql += ' from chadwickbureau '
+        sql += ' where key_person = \'' + str(self.uuid) + '\''
+
+        cur = conn.cursor()
+        cur.execute(sql)
+
+        year = 0
+        month = 0
+        day = 0
+        first = 0
+        last = 0
+        for row in cur:
+            year = int(row[0])
+            month = int(row[1])
+            day = int(row[2])
+            first = int(row[3])
+            last = int(row[4])
+           
+        with self.output().open('w') as out_file:
+            for yr in range(first, last+1):
+                age = yr - year - 1
+                if month <= 6:
+                    age += 1
+
+                out_file.write(str(yr) + "," + str(age) + '\n')
+
+
+    def output(self):
+        return luigi.LocalTarget('/home/dan/data/age/age_seasons_{0}.csv'.format(str(self.uuid)))
+
 class WarSeasons(luigi.Task):
     uuid = luigi.Parameter()
 
     def run(self):
         br = BRSource()
-        wardict = br.getBatWar(self.uuid)
+        war = br.getBatWar(self.uuid)
 
         with self.output().open('w') as out_file:
-            for key in sorted(wardict):
-                out_file.write(key + "," + str(wardict[key]) + '\n')
+            for year in war.years:
+                bat_war = war._year_wars[year].bat_war
+                out_file.write(str(year) + "," + str(bat_war) + '\n')
 
     def output(self):
-        return luigi.LocalTarget('/home/dan/data/total_war_{0}.csv'.format(str(self.uuid)))
+        return luigi.LocalTarget('/home/dan/data/war_by_year_{0}.csv'.format(str(self.uuid)))
+
+
+class TestTask(luigi.Task):
+    def requires(self):
+        return AgeSeasons('f322d40f')
+
+    def run(self):
+        print "hi"
+
+    def output(self):
+        return luigi.LocalTarget('home/dan/data/test.txt')
 
 class WarList(luigi.Task):
     def requires(self):
